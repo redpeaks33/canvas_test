15d5c699dc1300be8989dc3fdb2a5b025c106e1c
butanokakuni <redpeaks33@gmail.com>
2020-06-30 18:00:55 +0900

    Initial commit

---
+++ canvas_test/JavaScript.js
@@ -0,0 +1,148 @@
+﻿
+let plots = [
+    { id: 1, name: 'aaaaa', rate: 1},
+    { id: 2, name: 'bbbbb', rate: 1},
+    { id: 3, name: 'ccccc', rate: 0.5},
+    { id: 4, name: 'ddddd', rate: 0.3},
+    { id: 5, name: 'eeeee', rate: 1},
+]
+
+
+
+let canvas;
+let stage;
+let clock;
+let bg;
+let points = [];
+let images = [];
+let centerPos = {x:0,y:0}
+let LIGHT_GREEN = '#00FF00'
+
+//window.onresize = fitCanvasSize;
+//function fitCanvasSize() {
+//    let canvasSize = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight);
+//    canvas.width = canvasSize;
+//    canvas.height = canvasSize;
+//}
+
+function setStage() {
+    canvas = document.getElementById("radar-chart");
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#FF0000";
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    stage = new createjs.Stage(canvas);
+
+    radarContainer = new createjs.Container();
+    stage.addChild(radarContainer);
+    stage.update();
+}
+
+let init = function(){
+    setStage();
+
+    clock = new createjs.Container();
+    clock.x = canvas.width / 2;
+    clock.y = canvas.height / 2;
+    stage.addChild(clock);
+
+    let radius = canvas.width / 2;
+    for (var i = 1; i <= 3; i++) {
+        //#region circle
+        let circle = new createjs.Shape();
+        circle.graphics.setStrokeStyle(5).beginStroke(LIGHT_GREEN).drawCircle(0, 0, radius*i/3);
+        clock.addChild(circle);
+        //#endregion
+
+        //#region circle name
+        let text = new createjs.Text(72 * i / 3 + "h", "24px serif", LIGHT_GREEN);
+        text.x = 0;
+        text.y = -radius * i / 3;
+        text.textAlign = "center";
+        text.textBaseline = "top";
+        clock.addChild(text);
+        //#endregion
+    }
+
+    for (var i = 0; i < plots.length; i++) {
+        let line = new createjs.Shape();
+
+        let angle = i * (360 / plots.length) - 90;//rotation 90 degree
+        let radian = angle * Math.PI / 180;
+
+        //start point 
+        centerPos.x = 0 * Math.cos(radian);
+        centerPos.y = 0 * Math.sin(radian);
+
+        //end point 
+        let endX = radius * Math.cos(radian);
+        let endY = radius * Math.sin(radian);
+
+        //#region line
+        line.graphics
+            .setStrokeStyle(2)
+            .beginStroke(LIGHT_GREEN)
+            .moveTo(centerPos.x, centerPos.y)
+            .lineTo(endX, endY);
+
+        clock.addChild(line);
+        //#endregion
+
+        //images
+        //let bmp = new createjs.Bitmap("image2.gif");
+        //clock.addChild(bmp);
+
+        //bmp.x = endX * plots[i].rate;
+        //bmp.y = endY * plots[i].rate;
+        //bmp.scaleX = 0.1;
+        //bmp.scaleY = 0.1;
+        /*
+        bmp.regX = 200;
+        bmp.regY = 200;
+        bmp.radian = radian;
+        //let x = bmp.getBounds().width / 2;
+        //let y = bmp.getBounds().height / 2;
+        images[i] = bmp;
+        */
+
+        //#regin point
+        let point = new createjs.Shape();
+        point.radian = radian;
+        let xPos = endX * plots[i].rate;
+        let yPos = endY * plots[i].rate;
+        point.x = endX * plots[i].rate;
+        point.y = endY * plots[i].rate;
+        point.graphics
+            .beginFill("red")
+            .drawCircle(0, 0, 10);
+
+        points.push(point);
+        clock.addChild(point);
+        //#endregion
+
+        //#region point name
+        let title = new createjs.Text(plots[i].name, "24px serif", LIGHT_GREEN);
+        title.x = point.x;
+        title.y = point.y - 15;
+        title.textAlign = "center";
+        title.textBaseline = "bottom";
+        clock.addChild(title);
+        //#endregion
+    }
+
+    createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener("tick", stage);
+}
+
+let handleTick = function () {
+    //for (var i = 0; i < points.length; i++) {
+    //    if (Math.abs(points[i].x - centerPos.x) >= 1 || Math.abs(points[i].y - centerPos.y) >= 1) {
+    //        points[i].x -= 1 * Math.cos(points[i].radian);
+    //        points[i].y -= 1 * Math.sin(points[i].radian);
+    //    }
+    //}
+    //canvas.width = window.innerWidth;
+}
---
+++ createjs_test/JavaScript.js
@@ -0,0 +1,52 @@
+﻿
+let canvas;
+let stage;
+let clock;
+let bg;
+let points = [];
+let images = [];
+let centerPos = {x:0,y:0}
+
+function init() {
+    var stage = new createjs.Stage("demoCanvas");
+    stage.autoClear = false;
+    var line = new createjs.Shape();
+    line.graphics
+        .setStrokeStyle(12, "round") // 線幅(端を角丸に)
+        .beginStroke("gray")
+        .moveTo(0, 0)
+        .lineTo(0, -120); // 垂直上方向に線を描く
+    //circle.graphics.beginFill("Crimson").drawLine(0, 0, 50,50);
+    line.x = 400;
+    line.y = 400;
+    stage.addChild(line);
+    //createjs.Tween.get(line, { loop: false })
+    //    .to({ rotation: 360 }, 2000)
+
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", stage);
+    createjs.Ticker.addEventListener("tick", handleTick);
+}
+
+let r = 0;
+let handleTick = function () {
+    //for (var i = 0; i < points.length; i++) {
+    //    if (Math.abs(points[i].x - centerPos.x) >= 1 || Math.abs(points[i].y - centerPos.y) >= 1) {
+    //        points[i].x -= 1 * Math.cos(points[i].radian);
+    //        points[i].y -= 1 * Math.sin(points[i].radian);
+    //    }
+    //}
+    //canvas.width = window.innerWidth;
+    // 薄く暗く塗る
+    var line = new createjs.Shape();
+    line.graphics
+        .setStrokeStyle(1, "round") // 線幅(端を角丸に)
+        .beginStroke("gray")
+        .moveTo(0, 0)
+        .lineTo(0, -120+r); // 垂直上方向に線を描く
+    //circle.graphics.beginFill("Crimson").drawLine(0, 0, 50,50);
+    line.x = 400;
+    line.y = 400;
+    line.rotation = r++;
+    stage.addChild(line);
+}
